8f90a8732acb16abd37fa9ef18df49d363ce9c98
davinhonamorador <alenordavid@gmail.com>
2024-08-23 11:05:37 -0300

    finalizado

--- src/main/sorter/MergeSort.java
+++ src/main/sorter/MergeSort.java
@@ -5,10 +5,39 @@ public class MergeSort implements Sorter {
     @Override
     public int[] sort(int[] values) {
         // mergesort implementation
-        return values;
+        if (values.length <= 1) {
+            return values;
+        }
+        int mid = values.length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[values.length - mid];
+
+        System.arraycopy(values, 0, leftArray, 0, mid);
+        System.arraycopy(values, mid, rightArray, 0, values.length - mid);
+
+        return merge(sort(leftArray), sort(rightArray));
     }
 
     public int[] merge(int[] left, int[] right) {
-        return null;
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
     }
 }
--- src/main/sorter/QuickSorter.java
+++ src/main/sorter/QuickSorter.java
@@ -3,11 +3,59 @@ package sorter;
 public class QuickSorter implements Sorter {
     public int[] sort(int[] values) {
         // quicksort implementation
+        quickSort(values, 0, values.length - 1);
         return values;
     }
 
+    private void quickSort(int[] values, int low, int high) {
+        if (low < high) {
+            int pi = partitionsort(values, low, high);
+            quickSort(values, low, pi - 1);
+            quickSort(values, pi + 1, high);
+        }
+    }
+
+    public int partitionsort(int[] values, int low, int high) {
+        int pivot = values[high];
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (values[j] < pivot) {
+                i++;
+                int temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+            }
+        }
+        int temp = values[i + 1];
+        values[i + 1] = values[high];
+        values[high] = temp;
+
+        return i + 1;
+    }
+
     public int partition(int[] values, int low, int high) {
-        return 0;
+        int pivo = values[low];
+        int left = low + 1;
+        int right = high;
+
+        while (left <= right) {
+            while (left <= right && values[left] <= pivo) {
+                left++;
+            }
+            while (left <= right && values[right] > pivo) {
+                right--;
+            }
+            if (left < right) {
+                int temp = values[left];
+                values[left] = values[right];
+                values[right] = temp;
+            }
+        }
+        values[low] = values[high];
+        values[high] = pivo;
+
+        return right;
     }
     
 }
